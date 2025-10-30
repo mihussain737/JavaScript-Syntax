9e2d1274a67212b896b6f3c66b7e94d793b814bd
Md Imam Hussain <mihussain737@gmail.com>
2025-10-30 21:43:44 +0530

    data types added

---
+++ 2.Datatypes/index.js
@@ -0,0 +1,44 @@
+/*
+// primitive data types
+
+let name = "Imam";        // String
+let age = 25;             // Number
+let isDeveloper = true;   // Boolean
+let salary;               // Undefined
+let project = null;       // Null
+let id = Symbol("empId"); // Symbol
+let bigNum = 123456789012345678901n; // BigInt
+
+console.log(typeof name); // "string"
+console.log(typeof age);  // "number"
+
+// non primitive data types
+
+let person = {
+  name: "Imam",
+  age: 25
+};
+console.log(person)
+
+
+let numbers = [1, 2, 3, 4];
+console.log(numbers)
+
+
+function greet() {
+  console.log("Hello JavaScript!");
+}
+greet()
+*/
+
+let age=30;
+console.log(typeof age) // number
+console.log(age)
+
+age="40";
+console.log(typeof age) // string
+console.log(age)
+
+age=true;
+console.log(typeof age) //boolean
+console.log(true)

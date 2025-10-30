412ee2ee6f5741e642064be53c2383efeec9f202
Md Imam Hussain <mihussain737@gmail.com>
2025-10-30 22:24:34 +0530

    operator added

---
+++ 3.Operators/index.js
@@ -0,0 +1,53 @@
+// 1. Arithmatic Operators
+
+// let a=10;
+// let b=30;
+// console.log(a+b); // 40
+// console.log(b-a); // 20
+// console.log(a*b); //3300
+// console.log(b/a); // 3
+// console.log(b%a); // 0
+
+// 2. Assignment Operators
+
+// let a=10;
+// let b;
+// b=a;
+// // console.log(b) // a
+// b=b+10;
+// console.log(b) // 20
+
+// b+=10;
+// console.log(b)
+
+// b-=10;
+// console.log(b)
+
+// b*=10;
+// console.log(b)
+
+
+// 3. Comparison Operator
+
+// console.log(5==5) //true
+// console.log(6==5) // false
+
+
+// console.log(5=='5') // true
+
+// console.log(5==='5') // false
+
+// console.log(typeof 5)
+// console.log(typeof '5')
+
+// console.log('5'==='5')  //true
+
+// console.log(5!=='5') //true
+// console.log('5'!=='6') // true
+// console.log('5'!=='5')  // false
+
+//4. Ternary Operator
+
+let age=30;
+const status=age>=18?"adult":"baccha hai";
+console.log(status)

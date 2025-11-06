7d80feba0436568ecdd07f450b8203e1ad1339a4
Md Imam Hussain <mihussain737@gmail.com>
2025-11-06 09:24:55 +0530

    callback methods added

--- 15.Callbacks/index.js
+++ 15.Callbacks/index.js
@@ -1,3 +1,5 @@
+// returning example
+
 // console.log("Immediate run");
 
 
@@ -6,16 +8,128 @@
 // }, 2000);
 
 
+// Need of call back
+// function fetchData(){
+//      setTimeout(()=>{
+//           console.log("Data from server...")
+//           return "returned data"
+//      })
+// }
 
-function fetchData(){
+// function processData(){
+//      let data=fetchData();
+//      console.log(data);  // undefined
+//                          // Data from server
+// }
+
+// processData();  
+
+
+// Non anonymous function
+/*
+function fetchDataWithCallback(callback){
      setTimeout(()=>{
-          console.log("Data from server...")
-     })
+               let data='sample data';
+               callback(data)
+     },3000)
 }
 
-function processData(){
-     let data=fetchData();
-     console.log(data);  // undefined
+function processDataWithCallback(data){
+     console.log('With Callback '+data);
 }
 
-processData();  
+fetchDataWithCallback(processDataWithCallback); 
+
+
+
+// Anonymous callback functions
+
+function fetchDataWithCallbackNonAny(callback){
+     setTimeout(()=>{
+               let data='sample data';
+               callback(data)
+     },4000)
+}
+
+
+fetchDataWithCallbackNonAny(function(data){
+     console.log("With NonAnonymous: "+ data)
+}); 
+*/
+
+
+// callback for success / errors
+/*
+function fetchDataWithSuccessError(successCallback, errorCallback){
+     setTimeout(()=>{
+          let errorOccurred=false;
+
+          if(errorOccurred){
+               errorCallback("Error Occurred")
+          }else{
+               let data='sample data';
+               successCallback(data)
+          }
+     },4000)
+}
+
+
+function onSuccess(data){
+     console.log("Success, ", data);
+     
+}
+
+function onError(data){
+     console.log("Error, ", data);
+     
+}
+
+
+fetchDataWithSuccessError((data)=>{
+     console.log("Success,",data);
+},(data)=>{
+     console.log("Error, ", data)
+});
+*/
+
+
+// Chain Function
+
+function step1(callback){
+     setTimeout(()=>{
+          console.log("Step 1 DONE");
+          callback()
+     },1000)
+}
+
+function step2(callback){
+     setTimeout(()=>{
+          console.log("Step 2 DONE");
+          callback()
+     },1000)
+}
+
+function step3(callback){
+     setTimeout(()=>{
+          console.log("Step 3 DONE");
+          callback()
+     },1000)
+}
+
+step1(()=>{
+     step2(()=>{
+          step3(()=>{
+               console.log("All Completed")
+          });
+     });
+});
+
+
+// use callbacks with array methods
+
+let numbers=[1,2,3,4,5,6]
+let doubledNumbers=numbers.map((num)=>{
+     return num*2;
+});
+
+console.log(doubledNumbers)

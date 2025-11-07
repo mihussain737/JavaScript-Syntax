1db51442fa6c7e9a0a5083c258522c6d446521c1
Md Imam Hussain <mihussain737@gmail.com>
2025-11-07 12:51:34 +0530

    error handling added

---
+++ 18.Error-Handling/index.js
@@ -0,0 +1,38 @@
+// try catch block to handle the exception occure on the block of code
+
+// 1.
+//a();  //index.js:4 Uncaught ReferenceError: a is not defined
+//console.log("executed fully") // this will not run because of above error not handle
+
+
+//2. try catch block to handle the error
+try{
+     a();
+     console.log("This line not execute");
+}catch(error){
+     console.error(error);  // index.js:12 ReferenceError: a is not defined
+     console.error(error.message); //index.js:13 a is not defined
+}
+console.log("executed fully") // now this will run because we handle the error
+
+
+//3. Finally block:- finally block always run either exception occured or not
+
+try {
+     a();
+     console.log("This will not run")
+} catch (error) {
+     console.log("This handle the message after error",error)
+}finally{
+     console.log("This is always run")
+}
+
+//4. try and finally:- we can use either try-catch or try-finally// only try will throw error
+
+try{
+     let a=10/0;
+     console.log(10/0) // Infinity
+}finally{
+     console.log("This will always run");
+}
+
